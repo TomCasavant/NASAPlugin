879563e97c4dd550f36b693f759bde1d0e0eaaa7
Tom Casavant <tfcasavant@gmail.com>
2019-05-20 23:04:44 -0400

    Fixed formatting

--- NASAPlugin.py
+++ NASAPlugin.py
@@ -3,11 +3,13 @@ from urllib.request import urlopen
 import json
 import os
 
+
 class NASAPlugin(IPlugin):
     def process(self, msg, client):
-        if ("!space" in msg['text']):
-            print("here")
-            response = urlopen(f"https://api.nasa.gov/planetary/apod?api_key={os.getenv("NASA_API_KEY")}")
+        if "!space" in msg["text"]:
+            response = urlopen(
+                f'https://api.nasa.gov/planetary/apod?api_key={os.getenv("NASA_API_KEY")}'
+            )
             js = json.load(response)
             response = f"{js['title']} {js['url']}"
             client.send_message(response)
