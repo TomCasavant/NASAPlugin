cbd1c1f2a6963758009cae2d25a375c00c7f24e4
Tom Casavant <tfcasavant@gmail.com>
2019-05-23 20:53:43 -0400

    Added dscover

--- NASAPlugin.py
+++ NASAPlugin.py
@@ -2,7 +2,8 @@ from yapsy.IPlugin import IPlugin
 from urllib.request import urlopen
 import json
 import os
-
+import random
+import io
 
 class NASAPlugin(IPlugin):
     def process(self, msg, client):
@@ -13,3 +14,12 @@ class NASAPlugin(IPlugin):
             js = json.load(response)
             response = f"{js['title']} {js['url']}"
             client.send_message(response)
+        if "!dscover" in msg["text"].lower():
+            response = urlopen(
+                f'https://api.nasa.gov/EPIC/api/natural/images?api_key={os.getenv("NASA_API_KEY")}'
+            )
+            js = random.choice(json.load(response))
+            image = urlopen(f"https://epic.gsfc.nasa.gov/archive/natural/{js['date'][0:4]}/{js['date'][5:7]}/{js['date'][8:10]}/jpg/{js['image']}.jpg")
+            data = io.BytesIO(image.read())
+            image_url = client.upload_image(data, "image/jpeg")
+            client.send_message_with_image(image_url, js['caption'])
